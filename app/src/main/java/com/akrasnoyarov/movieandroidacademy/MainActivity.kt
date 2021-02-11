882cb826837eee9bb4f65c9409b4448a6530bbe7
ghtt <krasnoyarov1990@gmail.com>
2021-02-11 18:32:39 +0300

    Homework #3 is finished

--- app/src/main/java/com/akrasnoyarov/movieandroidacademy/MainActivity.kt
+++ app/src/main/java/com/akrasnoyarov/movieandroidacademy/MainActivity.kt
@@ -3,8 +3,15 @@ package com.akrasnoyarov.movieandroidacademy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.akrasnoyarov.movieandroidacademy.fragments.MovieDetailsFragment
+import com.akrasnoyarov.movieandroidacademy.fragments.MovieListFragment
 
-class MainActivity : SingleFragmentActivity() {
-    override fun createFragment() = MovieDetailsFragment.newInstance()
+class MainActivity : SingleFragmentActivity(), MovieListFragment.Callbacks {
+    override fun createFragment() = MovieListFragment.newInstance()
 
+    override fun onMovieItemClicked() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MovieDetailsFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+    }
 }
---
+++ app/src/main/java/com/akrasnoyarov/movieandroidacademy/fragments/MovieListFragment.kt
@@ -0,0 +1,45 @@
+package com.akrasnoyarov.movieandroidacademy.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.akrasnoyarov.movieandroidacademy.R
+
+class MovieListFragment : Fragment() {
+    private var callback: Callbacks? = null
+
+    interface Callbacks {
+        fun onMovieItemClicked()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+        view.findViewById<ConstraintLayout>(R.id.movie_item)?.apply {
+            setOnClickListener {
+                callback?.onMovieItemClicked()
+            }
+        }
+
+        return view
+    }
+
+        override fun onAttach(context: Context) {
+            super.onAttach(context)
+            if (context is Callbacks) {
+                callback = context
+            }
+        }
+
+        companion object {
+            fun newInstance() = MovieListFragment()
+        }
+    }
