3509f9b759683777154108809979ef71d6e11560
ghtt <krasnoyarov1990@gmail.com>
2021-02-26 17:56:25 +0300

    Movie can be open by deeplink, e.g. https://movieandroidacademy.akrasnoyarov.com/movie/599281

--- app/src/main/java/com/akrasnoyarov/movieandroidacademy/MainActivity.kt
+++ app/src/main/java/com/akrasnoyarov/movieandroidacademy/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.akrasnoyarov.movieandroidacademy
 
+import android.content.Intent
+import android.util.Log
+import androidx.fragment.app.FragmentManager
 import com.akrasnoyarov.movieandroidacademy.adapters.MovieViewAdapter
 import com.akrasnoyarov.movieandroidacademy.fragments.MovieDetailsFragment
 import com.akrasnoyarov.movieandroidacademy.fragments.MovieListFragment
 
 class MainActivity : SingleFragmentActivity(), MovieViewAdapter.OnMovieClickListener {
     override fun createFragment() = MovieListFragment.newInstance()
+    override fun handleIntent(intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_VIEW -> {
+                val movieId = intent.data?.lastPathSegment?.toInt() ?: return
+                supportFragmentManager.popBackStack(
+                    MOVIE_TAG,
+                    FragmentManager.POP_BACK_STACK_INCLUSIVE
+                )
+                openMovieDetails(movieId, MOVIE_TAG)
+            }
+            else -> Log.d("myLogs", "Unknown action!")
+        }
+    }
 
     override fun onMovieItemClicked(movieId: Int) {
+        openMovieDetails(movieId)
+    }
+
+    private fun openMovieDetails(id: Int, tag: String? = null) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, MovieDetailsFragment.newInstance(movieId))
-            .addToBackStack(null)
+            .addToBackStack(tag)
+            .replace(R.id.fragment_container, MovieDetailsFragment.newInstance(id))
             .commit()
     }
+
+    companion object {
+        private const val MOVIE_TAG = "movie"
+    }
+
 }
--- app/src/main/java/com/akrasnoyarov/movieandroidacademy/SingleFragmentActivity.kt
+++ app/src/main/java/com/akrasnoyarov/movieandroidacademy/SingleFragmentActivity.kt
@@ -1,11 +1,13 @@
 package com.akrasnoyarov.movieandroidacademy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-abstract class SingleFragmentActivity: AppCompatActivity() {
+abstract class SingleFragmentActivity : AppCompatActivity() {
     abstract fun createFragment(): Fragment
+    abstract fun handleIntent(intent: Intent)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +18,11 @@ abstract class SingleFragmentActivity: AppCompatActivity() {
                 .add(R.id.fragment_container, createFragment())
                 .commit()
         }
+        intent?.let(::handleIntent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let(::handleIntent)
     }
 }
